2ca999ba0e33577ca703ad342a72a7269424a98e
preznick <preznick@gmail.com>
2017-02-28 07:49:47 +0000

    cleaned counter code from backend and frontend

--- main.rb
+++ main.rb
@@ -1,25 +1,8 @@
-require 'memcachier'
-require 'dalli'
 require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'tilt/erubis'
 
 require_relative 'haiku.rb'
-
-MONTHS = %w(Jan Feb Mar Apr May June July Aug Sep Oct Nov Dec)
-
-configure do
-  startup_date = settings.cache.get('start_date') || Time.now
-  set :startup_date, startup_date
-end
-
-def increment_count
-  CACHE.set('haiku_count', get_count + 1)
-end
-
-def get_count
-  CACHE.get('haiku_count') || 0
-end
 
 def get_haiku
   trump_path = './public/text_files/trump_speeches/trump-speeches-master/speeches.txt'
