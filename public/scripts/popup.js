cd17952fe3a45eddd0e1af3ab979744aa3e6d82c
Peter Reznick <preznick@gmail.com>
2017-05-30 20:50:45 -0400

    changed popup to modal box

--- public/scripts/popup.js
+++ public/scripts/popup.js
@@ -1,7 +1,36 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var popup_trigger = document.getElementById('popup_trigger');
-  popup_trigger.addEventListener('click', function(e) {
-    var popup = document.getElementById('popup');
-    popup.style.opacity == '0' ? popup.style.opacity = '1' : popup.style.opacity = '0';
+  var popupTrigger = document.getElementById('popup_trigger');
+  var xOut = document.getElementById('x_out');
+  var popupContainer = document.getElementById('popup_container');
+
+  function closePopup() {
+    popupContainer.style.display = 'none';
+  }
+
+  popupTrigger.addEventListener('click', function(e) {
+    console.log('clicked');
+    var display = popupContainer.style.display !== 'block';
+
+    if (display !== 'block' || display === '') {
+      popupContainer.style.display = 'block';
+    } else {
+      closePopup();
+    }
   });
+
+  document.addEventListener('keydown', function(e) {
+    if (e.keyCode === 27) {
+      closePopup();
+    }
+  });
+
+  xOut.addEventListener('click', function() {
+    closePopup();
+  });
+
+  popupContainer.addEventListener('click', function(e) {
+    if (e.target.tagName === 'DIV') {
+      popupContainer.style.display = 'none';
+    }
+  })
 });
