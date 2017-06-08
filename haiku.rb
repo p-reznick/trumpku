66696956932ae4a297eb9a7d1cbd843615be177a
Peter Reznick <preznick@gmail.com>
2017-06-08 14:23:32 -0400

    refactored format_haiku method

--- haiku.rb
+++ haiku.rb
@@ -11,33 +11,36 @@ class Haiku
 
   def get_sample_haiku
     opt = rand(3)
-    opt = 2
 
     case opt
     when 0
-      raw_haiku = phrases.get_phrase('start', 5) + "\n" +
-                  phrases.get_phrase('mid', 7) + "\n" +
-                  phrases.get_phrase('end', 5)
+      raw_haiku = phrases.get_splittable_text([5, 7, 5], [5, 7, 5])
     when 1
-      raw_haiku = phrases.get_sentence(5) + "\n" +
-                  phrases.get_sentence(7) + "\n" +
-                  phrases.get_sentence(5)
+      raw_haiku = phrases.get_splittable_text([5, 7, 5], [8, 9])
     when 2
-      phrases.get_splittable_text([5, 7, 5], [17])
+      raw_haiku = phrases.get_splittable_text([5, 7, 5], [17])
     end
 
     format_haiku(raw_haiku)
   end
 
   def format_haiku(text)
-    haiku = ''
-    text.split("\n").map do |line|
-      line[0] = line[0].upcase
-      line
+    clean_text = text.gsub(/\A\.\s+/, '').gsub(/\.\./, '.')
+    pattern = [5, 7, 5]
+    lines = []
+
+    clean_text.split.reduce('') do |aggregate, word|
+      aggregate.concat(' ' + word)
+
+      if (phrases.get_phrase_syllables(aggregate) === pattern.first)
+        lines.push(aggregate)
+        pattern.shift
+        aggregate = ''
+      end
+
+      aggregate
     end
+
+    lines
   end
 end
-
-# trump_path = './public/text_files/trump_speeches/trump-speeches-master/speeches.txt'
-# h = Haiku.new(trump_path)
-# binding.pry
